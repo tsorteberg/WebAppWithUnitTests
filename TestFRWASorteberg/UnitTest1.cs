7c37762fecce53299434d3472a936cbbf78bbd40
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-02-08 22:25:48 -0600

    Added TestNullValueName() Unit Test.

--- TestFRWASorteberg/UnitTest1.cs
+++ TestFRWASorteberg/UnitTest1.cs
@@ -1,3 +1,19 @@
+/***************************************************************
+* Name        : FRWAModel.cs
+* Author      : Tom Sorteberg
+* Created     : 02/08/2021
+* Course      : CIS 174
+* Version     : 1.0
+* OS          : Windows 10 Pro, Visual Studio Community 2019
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This unit test class file was created to test
+*               the FirstResponsiveWebAppSorteberg project for 
+*               CIS-174
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access 
+* to my program.         
+***************************************************************/
 using System;
 using Xunit;
 using FirstResponsiveWebAppSorteberg.Models;
@@ -45,6 +61,7 @@ namespace FRWASortebergTest
         [Fact]
         public void TestGetNameSuccess()
         {
+            //ARRANGE
             DateTime birthDate = new DateTime(1977, 10, 30);
             FRWAModel testObject = new FRWAModel
             {
@@ -62,6 +79,7 @@ namespace FRWASortebergTest
         [Fact]
         public void TestGetBirthDateSuccess()
         {
+            //ARRANGE
             DateTime birthDate = new DateTime(1977, 10, 30);
             FRWAModel testObject = new FRWAModel
             {
@@ -74,6 +92,18 @@ namespace FRWASortebergTest
 
             //ASSERT
             Assert.Equal(birthDateTest, testObject.BirthDate);
+        }
+
+        [Fact]
+        public void TestNullValueName()
+        {
+            //ARRANGE
+            FRWAModel testObject = new FRWAModel();
+
+            //ACT
+
+            //ASSERT
+            Assert.Null(testObject.Name);
         }
     }
 }
